80e71c095459ae69bd8b0c6874e2aab91ad74287
Juszcze Krzysztof <krzjus@live.com>
2018-03-17 14:26:11 +0100

    StartProject

---
+++ The explosion hill/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+
+    Rigidbody Rigidbody;
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody.AddForce(transform.right * speed);
+        }
+	}
+}
